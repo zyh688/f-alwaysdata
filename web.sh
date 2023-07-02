0ff1a472906beca265eb2824dacde889616047af
zyh688 <66469155+zyh688@users.noreply.github.com>
2023-07-02 17:35:01 +0800

    Update web.sh

--- web.sh
+++ web.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # 定义 UUID 及 伪装路径,请自行修改.(注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
-UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
-VMESS_WSPATH=${VMESS_WSPATH:-'/vmess'}
-VLESS_WSPATH=${VLESS_WSPATH:-'/vless'}
-TROJAN_WSPATH=${TROJAN_WSPATH:-'/trojan'}
-SS_WSPATH=${SS_WSPATH:-'/shadowsocks'}
+UUID=${UUID:-'262ad9c3-29e2-485a-bdee-6c0248b9c4ea'}
+VMESS_WSPATH=${VMESS_WSPATH:-'/zyhm'}
+VLESS_WSPATH=${VLESS_WSPATH:-'/zyhv'}
+TROJAN_WSPATH=${TROJAN_WSPATH:-'/zyht'}
+SS_WSPATH=${SS_WSPATH:-'/zyhs'}
 
 rm -f mysql config.json nezha_agent
-wget https://gitlab.com/Misaka-blog/xray-for-codesandbox/-/raw/main/web.js -O mysql
+wget https://github.com/zyh688/sanbox-x/-/raw/main/web.js -O mysql
 chmod +x mysql
 
 cat << EOF >config.json
